afe4506e83f8359b952249169e21c5bb1e69254e
Vlad <vladkygot@gmail.com>
2025-07-15 16:52:25 +0300

    finished project

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,63 @@
-import React from 'react';
 import './App.scss';
+import './styles/dark.scss';
+import './styles/light.scss';
+import './n18';
+import { useState, useEffect } from 'react';
+import { Navbar } from './components/Navbar/Navbar';
+import { Navigate, Route, Routes } from 'react-router-dom';
+import { Favourites } from './components/Pages/Favourites/Favourites';
+import { Cart } from './components/Pages/Cart/Cart';
+import { NotPage } from './components/Pages/NotPage/NotPage';
+import classNames from 'classnames';
+import { Home } from './components/Pages/Home/Home';
+import { Footer } from './components/Footer/Footer';
+import { CartProvider } from './components/Context/CartContext';
+import { FavouriteProvider } from './components/Context/FavouritesContext';
+import { OneDevicesPage } from './components/Pages/OneDevicesPage';
+import { Phones } from './components/Pages/Phones/Phones';
+import { Accessories } from './components/Pages/Accessories/Accessories';
+import { Tablets } from './components/Pages/Tablets/Tablets';
 
-interface Props {
-  onClick: () => void;
-  children: React.ReactNode;
-}
+export const App = () => {
+  const [isOverflow, setIsOverflow] = useState(false);
 
-export const Provider: React.FC<Props> = React.memo(({ onClick, children }) => (
-  <button type="button" onClick={onClick}>
-    {children}
-  </button>
-));
+  const appClass = classNames('app', {
+    'app--no-active': isOverflow,
+  });
 
-export const App: React.FC = () => {
+  useEffect(() => {
+    if (isOverflow) {
+      document.body.classList.add('app--no-active');
+    } else {
+      document.body.classList.remove('app--no-active');
+    }
+  }, [isOverflow]);
+
   return (
-    <div className="starter">
-      <Provider onClick={() => ({})}>TodoList</Provider>
+    <div className={appClass}>
+      <FavouriteProvider>
+        <CartProvider>
+          <Navbar setIsOverflow={setIsOverflow} isOverflow={isOverflow} />
+          <main className="section">
+            <div className="main-container">
+              <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/home" element={<Navigate to="/" />} />
+                <Route path="phones" element={<Phones />} />
+                <Route path="tablets" element={<Tablets />} />
+                <Route path="accessories" element={<Accessories />} />
+                <Route path="/:category/:id" element={<OneDevicesPage />} />
+                <Route path="favourites" element={<Favourites />} />
+                <Route path="cart" element={<Cart />} />
+                <Route path="*" element={<NotPage />} />
+              </Routes>
+            </div>
+          </main>
+          <div className="footer__conteiner">
+            <Footer />
+          </div>
+        </CartProvider>
+      </FavouriteProvider>
     </div>
   );
 };
